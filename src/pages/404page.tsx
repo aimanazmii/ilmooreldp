5f9a994f97b64f5792413c6e12941524e9c1e207
aimannazmii <d19311832@student>
2023-09-26 15:32:40 +0800

    update

--- src/pages/404page.tsx
+++ src/pages/404page.tsx
@@ -17,7 +17,7 @@ export default function Errorpage() {
                 </p>
                 <a href="/">
                   <button className="sm:w-full lg:w-auto my-2 border rounded md py-4 px-8 text-center bg-indigo-600 text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-700 focus:ring-opacity-50">
-                    Take me there and see you again
+                    Take me there !
                   </button>
                 </a>
               </div>
